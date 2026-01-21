39e2f79b654dfa003aad7f2b5a532421325ed806
nerdrama.ai <nerdrama.ai@gmail.com>
2026-01-22 02:02:09 +0530

    Update Tile.jsx

--- src/components/Tile.jsx
+++ src/components/Tile.jsx
@@ -1,22 +1,21 @@
 import { getAssetPath } from "../engine/assetResolver";
 
-export default function Tile({ data, onPointerDown, onPointerUp }) {
+export default function Tile({ data, onClick, selected, shake }) {
   if (!data) return <div className="tile empty" />;
 
-  const src = getAssetPath(data.type, data.color);
-  if (!src) return <div className="tile empty" />;
+  const className = `
+    tile
+    ${selected ? "selected" : ""}
+    ${shake ? "shake" : ""}
+  `;
 
   return (
-    <div
-      className="tile"
-      onPointerDown={onPointerDown}
-      onPointerUp={onPointerUp}
-    >
+    <div className={className} onClick={onClick}>
       <img
-        src={src}
+        src={getAssetPath(data.type, data.color)}
         className="item"
         draggable={false}
-        alt={`${data.color} ${data.type}`}
+        alt=""
       />
     </div>
   );
