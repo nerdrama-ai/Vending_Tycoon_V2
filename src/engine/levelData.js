51e4218cdb0f5d08154138a1aac118065af1cb6d
nerdrama.ai <nerdrama.ai@gmail.com>
2026-01-22 01:59:43 +0530

    Delete src/engine directory

--- src/engine/levelData.js
+++
@@ -1,5 +0,0 @@
-
-export const levels = [
-  { matches: 10, depth: 4, allowedItems: ["chips","chocolate","drink","can","bar"] },
-  { matches: 14, depth: 5, allowedItems: ["chips","drink","can"] }
-];
